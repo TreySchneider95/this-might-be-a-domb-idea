5fd12fba8dade0c77ddf67f1b0391c84fd100ddf
TreySchneider95 <tschneid95@gmail.com>
2022-10-24 14:18:44 -0500

    fixed heading issue

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector("p").style.color = "lightblue"
-document.querySelector(".heading").style.fontSize = "48px"
+document.querySelector("h1").style.fontSize = "48px"
 document.querySelector("p").innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
 document.querySelector("ul :nth-child(13)").style.opacity = "0.5"
 document.querySelector("ul :nth-child(3)").innerText = "Hi!"
